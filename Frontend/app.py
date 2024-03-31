52831081e3a559a94f6c7ec47edd765a84fb1a0c
Leo-QJ-Justin <157592511+Leo-QJ-Justin@users.noreply.github.com>
2024-03-31 18:05:24 +0800

    Update app.py
    Added navbar and nav_panels for 3 sub-problems. Applied minty theme from shinyswatch

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,50 +1,67 @@
-from shiny import App, render, ui,Inputs,Outputs,Session
+from shiny import App, Inputs, Outputs, Session, reactive, render, ui
+import shinyswatch
 from shinywidgets import output_widget, render_widget
+#import shinyswatch
+from htmltools import HTMLDependency
 import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
 import os
 import geopandas as gpd
-import helper_functions as helper
 from shapely import Point 
 import json 
+from bs4 import BeautifulSoup
+from pathlib import Path
 
-
-
-# path_metrics = pd.read_csv(os.path.join(data_directory, 'path_metrics.csv'))
-
-# path_metrics = path_metrics.astype(str)
-
-
-app_ui = ui.page_fluid(
-    ui.page_navbar(title = "DSE3101 Cycle",
-                   bg= "#32de84"),
-    ui.h2("Rankings by Planning Area"),
-    ui.input_select("metrics", "Select metric for comparison", choices=["suitability", "time savings", "weighted score"],selected = "suitability"),
-    ui.layout_columns(
-    ui.card(output_widget("chloropeth_map")),
-    ui.card(),
-    col_widths = (8,4)
-    )   
-    # ui.layout_columns(
-    #     output_widget("chloropeth_map"),
-    #     output_widget("mrt_table"),
-    #     height ="300px")
-)
+app_ui = ui.page_navbar(
+    shinyswatch.theme.minty(),
+    ui.nav_panel("Sub-Problem 1: Cycling infrastructure suitability index","a"),
+    ui.nav_panel("Sub-Problem 2: Last Mile Acessibility Index",
+                 ui.h2("Rankings by Planning Area"),
+                 ui.input_select("metrics", "Select metric for comparison", 
+                                 choices=["suitability", "time savings", "weighted score"],
+                                 selected = "suitability"),
+                ui.layout_columns(
+                    ui.card(
+                        output_widget("chloropeth_map")
+                    ),
+                    ui.card(
+                    )
+                )   
+    ),
+    ui.nav_panel("Sub-Problem 3: Isochrone Analysis",
+                 "c"
+    ),
+    title = "DSE3101 Cycle",
+    bg= "#20c997"
+    )
 
 def server(input, output, session):
+
+    def extract_td_contents(html):
+        soup = BeautifulSoup(html, 'html.parser')
+        table = soup.find('table')
+        rows = table.find_all('tr')
+
+        td_contents = []
+        for row in rows:
+            cells = row.find_all('td')
+            for cell in cells:
+                td_contents.append(cell.text.strip())
+
+        return td_contents
 
     current_directory = os.getcwd()
     data_directory = os.path.join(current_directory, 'data')
 
     basemap = gpd.read_file(os.path.join(data_directory, 'MasterPlan2019PlanningAreaBoundaryNoSea.geojson'))
-    basemap['Planning_Area'] = basemap["Description"].apply(lambda x: helper.extract_td_contents(x)[0])
+    basemap['Planning_Area'] = basemap["Description"].apply(lambda x:extract_td_contents(x)[0])
     basemap['geometry'] = basemap['geometry'].to_crs("4326")
 
-    mrt_stations_df = pd.read_csv(os.path.join(data_directory, 'mrt_station_final.csv'),usecols = [1,2,3])
+    mrt_stations_df = pd.read_csv(os.path.join(data_directory, 'Cluster_data','mrt_station_final.csv'),usecols = [1,2,3])
     mrt_stations_df.sort_values(by='MRT.Name', inplace=True)
-    ranking = pd.read_csv(os.path.join(data_directory, 'mrt_ranking.csv'))
+    ranking = pd.read_csv(os.path.join(data_directory,'Cluster_data','mrt_ranking.csv'))
 
     mrt_stations_df_combined = pd.merge(mrt_stations_df, ranking, left_on='MRT.Name', right_on='MRT.Name', how='left')
     for index, row in mrt_stations_df_combined.iterrows():
@@ -72,6 +89,13 @@ def server(input, output, session):
                                    hoverinfo = 'text',
                                    text = ("Planning Area: " + basemap_modified['Planning_Area'] + '<br>' + 
                                            "Average " + input.metrics() + " of paths within the area: " + basemap_modified[input.metrics()].astype(str) )))
+        
+        fig.add_trace(go.Scattergeo(geojson=json.loads(basemap_modified.geometry.to_json()),
+                                    locations = basemap_modified.index,
+                                    featureidkey= 'properties.index',
+                                    text = basemap_modified['Planning_Area'],
+                                    mode = 'text',))
+
         fig.update_layout(
             margin={'l':0,'t':0,'b':0,'r':0},
             mapbox={
@@ -91,7 +115,5 @@ app = App(app_ui, server)
 
 if __name__ == "__main__":
     app.run()
-
-
 
 
